327e8dacd3f18ea3ab24bdb1b2f614141da4dece
Freddie Brown <fred@noser.net>
2020-01-10 16:22:50 +0000

    trying to get it running on VM

--- code/simplescan.c
+++ code/simplescan.c
@@ -1,18 +1,4 @@
-#include <stdio.h>
-#include <errno.h>
-#include <ctype.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <getopt.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <signal.h>
-
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/hci_lib.h>
+#include "discover.h"
 
 int discover();
 
