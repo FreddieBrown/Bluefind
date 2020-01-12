6b778ac871c6693eadbc7c1c3efabb90500d46d5
Freddie Brown <fred@noser.net>
2020-01-12 13:52:07 +0000

    Changed Makefile and testing GDBUS code v5

--- code/src/discover.h
+++ code/src/discover.h
@@ -14,8 +14,7 @@
 
 #include <glib.h>
 
-#include "gdbus/gdbus.h"
-// #include <gio/gio.h>
+#include <gio/gio.h>
 // #include <dbus/dbus.h>
 
 #include <bluetooth/bluetooth.h>
@@ -26,6 +25,13 @@
 #define COLORED_CHG	COLOR_YELLOW "CHG" COLOR_OFF
 #define COLORED_DEL	COLOR_RED "DEL" COLOR_OFF
 
-int discover();
+void name_appeared(GDBusConnection *connection,
+                   const gchar *name,
+                   const gchar *name_owner,
+                   gpointer user_data);
+void name_vanished(GDBusConnection *connection,
+                   const gchar *name,
+                   gpointer user_data);
+
 
 #endif
--- code/src/main.c
+++ code/src/main.c
@@ -1,14 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include<glib.h>
-#include <bluetooth/bluetooth.h>
 #include "discover.h"
-int main(){
+int main(void)
+{
+    GMainLoop *loop;
+    GError *error;
+    guint name;
 
-    printf("This is the project!\n"); 
-    discover(); 
-    DBusMessageIter value; 
-    return 1;
+    loop = g_main_loop_new(NULL, FALSE);
+    name = g_bus_watch_name(G_BUS_TYPE_SYSTEM,
+                            "org.bluez", // 
+                            G_BUS_NAME_WATCHER_FLAGS_NONE,
+                            name_appeared,
+                            name_vanished,
+                            NULL,
+                            NULL);
+
+    g_main_loop_run(loop);
 }
