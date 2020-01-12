99863105a9c9b57972aaa2582d7be036c2a4224e
Freddie Brown <fred@noser.net>
2020-01-12 14:32:30 +0000

    Adding DBUS lib to project

--- code/src/bluefind.h
+++ code/src/bluefind.h
@@ -15,11 +15,7 @@
 #include <glib.h>
 
 #include <gio/gio.h>
-// #include <dbus/dbus.h>
-
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/hci_lib.h>
+#include <dbus/dbus.h>
 
 #define COLORED_NEW	COLOR_GREEN "NEW" COLOR_OFF
 #define COLORED_CHG	COLOR_YELLOW "CHG" COLOR_OFF
