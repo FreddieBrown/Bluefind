cb551876fd8a2e73026cc7eb37239f373d6bcf99
Freddie Brown <fred@noser.net>
2020-01-12 06:27:30 -0800

    Got example working finally

--- code/src/main.c
+++ code/src/main.c
@@ -1,22 +1,6 @@
-// #include "discover.h"
+#include "discover.h"
 #include <gio/gio.h>
 #include <stdio.h>
-
-void name_appeared(GDBusConnection *connection,
-                   const gchar *name,
-                   const gchar *name_owner,
-                   gpointer user_data)
-{
-    printf("Name appeared: %s\n", name);
-    printf("Owned by: %s\n", name_owner);
-}
-
-void name_vanished(GDBusConnection *connection,
-                   const gchar *name,
-                   gpointer user_data)
-{
-    printf("Name vanished: %s\n", name);
-}
 
 int main(void)
 {
