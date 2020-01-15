b80a0da6ae03b0c4e1261971fdbae3a0c9598351
Freddie Brown <fred@noser.net>
2020-01-15 23:20:30 +0000

    working out what to do

---
+++ code/python/adapter_controls.py
@@ -0,0 +1,2 @@
+# This file will be for functions which will mutate properties of the adapter
+# e.g things like enabling discoverability
---
+++ code/python/gatt.py
@@ -0,0 +1,3 @@
+# This file will be used to register a GATT service which will 
+# be advertised to other devices
+# This will also contain GATT server code too
