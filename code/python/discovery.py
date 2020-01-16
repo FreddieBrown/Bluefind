7d1b47fcb491f0518aaa8fed2bcf249941e0952c
Freddie Brown <fred@noser.net>
2020-01-16 13:18:30 +0000

    Fixed printing byte issue

--- code/python/discovery.py
+++ code/python/discovery.py
@@ -27,7 +27,7 @@ def print_info(address, properties):
 				print("    %s = %s" % ("ManufacturerData Key", str(key)))
 				ManuData = "    ManufacturerData Value = "
 				for mandata in arr:
-					ManuData += str(mandata, 'utf-8')+" "
+					ManuData += str(int(mandata))+" "
 				print(ManuData)
 		else:
 			print("    %s = %s" % (key, value))
