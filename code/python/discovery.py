71677d4586758d7041ab4167d934ada715c27501
Freddie Brown <fred@noser.net>
2020-01-16 14:09:43 +0000

    adding stuff to prop changed

--- code/python/discovery.py
+++ code/python/discovery.py
@@ -55,7 +55,11 @@ def interfaces_added(path, interfaces):
 	print_info(address, devices[path])
 
 def properties_changed(interface, changed, invalidated, path):
-	if interface != "org.bluez.Device1":
+	if interface == "org.bluez.Adapter1":
+		for key, val in changed.items():
+			print("\n\t%s = %s\n", key, val)
+
+	elif interface != "org.bluez.Device1":
 		return
 
 	if path in devices:
