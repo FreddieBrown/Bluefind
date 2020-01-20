7da4ccde2f63c37d4a698acad929bc1193e02a68
Freddie Brown <fred@noser.net>
2020-01-20 16:12:41 +0000

    debugging

--- code/python/bluefind.py
+++ code/python/bluefind.py
@@ -82,6 +82,9 @@ if __name__ == '__main__':
 	adapter.SetDiscoveryFilter(scan_filter)
 	adapter.StartDiscovery()
 
+	print(adapter)
+	print(BLUEZ_SERVICE_NAME)
+
 	ad_manager = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, adapter), LE_ADVERTISING_MANAGER_IFACE)
 
 	em_advertisement = advertising.EmergencyAdvertisement(bus, 0)
