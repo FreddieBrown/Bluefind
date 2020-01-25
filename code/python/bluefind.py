147a67ec1d1c6eaed1383742b933a1a3c4c12c4c
Freddie Brown <fred@noser.net>
2020-01-25 13:15:10 +0000

    Getting options to work

--- code/python/bluefind.py
+++ code/python/bluefind.py
@@ -10,7 +10,7 @@ from gi.repository import GLib
 
 import bluezutils, discovery, advertising, gatt_server
 
-startup = "s"
+startup = None
 
 BLUEZ_SERVICE_NAME = 'org.bluez'
 LE_ADVERTISEMENT_IFACE = 'org.bluez.LEAdvertisement1'
