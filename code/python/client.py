2f0db39fd6b1e8824c8ae2fc3eb5a1262eb80e2d
Freddie Brown <fred@noser.net>
2020-02-03 16:42:39 +0000

    Created basic client functionality

--- code/python/client.py
+++ code/python/client.py
@@ -10,5 +10,44 @@ import dbus.service
 import array
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
+from gattlib import GATTRequester, GATTResponse
 
 import bluezutils, exceptions
+
+
+class Client():
+	SERVICE_UUID =  '0000FFF0-0000-1000-8000-00805f9b34fb'
+	RW_HANDLE = 0xFFF1
+
+	def __init__(self):
+		self.requester = None
+
+	def connect_to_device(self, address):
+		self.requester = GATTRequester(address)
+		self.requester.connect(True)
+		
+		data = self.read_value(requester)
+
+		return requester, data
+
+	def write_value(self, data):
+		if not self.requester:
+			print("Cannot write as no device to send to")
+		else:
+			self.requester.write_by_handle(self.RW_HANDLE, data)
+
+	def read_value(self):
+		if not self.requester:
+			print("Cannot write as no device to read from")
+			return None
+		else:
+			response = GATTResponse()
+
+			self.requester.read_by_handle_async(self.RW_HANDLE, response)
+			while not response.received():
+				time.sleep(0.1)
+			
+			return response.received()[0]
+
+
+
