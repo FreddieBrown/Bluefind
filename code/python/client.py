8254cb271b9c0dd986c76e00efbf3d88372b6752
Freddie Brown <fred@noser.net>
2020-02-06 23:00:12 +0000

    written method to write whole message
    Written method to write a whole message to server. Still needs to be
    tested

--- code/python/client.py
+++ code/python/client.py
@@ -21,19 +21,18 @@ class Client():
 		self.requester = None
 		self.target_address = None
 		self.discovery = DiscoveryService("hci0")
+		self.message = None
 
 	def prepare_device(self, target_address, connect=True):
 		self.requester = GATTRequester(target_address, connect)
 		self.target_address = target_address
-		data = self.read_value()
-		return data
 
 	def write_value(self,data):
 		if not self.requester:
 			print("Cannot write as no device to send to")
 		else:
 			print("Writing data")
-			for i in range(0x0001, 0xff):
+			for i in range(1, 100):
 				self.requester.write_cmd(i, data)
 
 
@@ -85,8 +84,41 @@ class Client():
 			
 		print("Characteristics for {}: {}".format(self.target_address, response.received()))
 		return response.received()
+	
 	def update_location(self, location):
 		self.location = location
+
+	def set_message(self, message):
+		self.message = message
+	
+	def send_message(self):
+		if not self.requester:
+			print("No connected device so cannot write message")
+			return None
+		elif not self.message:
+			print("Need to provide a message to send")
+			return None
+		else:
+			message_buffer = bluezutils.split_message(self.message)
+			for i in message_buffer:
+				cli.write_value(i)
+			print("Written whole message to {}".format(self.target_address))
+	
+	def read_message(self):
+		if not self.requester:
+			print("No connected device so cannot read message")
+			return None
+		message = []
+		while True:
+			data = bluezutils.from_byte_array(self.read_value())
+			if str(chr(5)) not in data:
+				message.append(data)
+			else:
+				message.append(data.strip(str(chr(5))))
+				break
+		print("Read whole message from {}".format(self.target_address))	
+		return ''.join(message)
+		
 
 cli = Client("52.281799, -1.532315", bluezutils.get_mac_addr(dbus.SystemBus()))
 
@@ -120,20 +152,29 @@ if __name__ == '__main__':
 	while True:
 		# Build message at start of each iteration
 		message = bluezutils.build_message([cli.location], [cli.device_address])
+		cli.set_message(message)
+		# Search for devices
 		devices = cli.discover(5)
+		# Go through discovered devices and print them
 		for address, name in list(devices.items()):
 			print("Device: {} {}".format(address, name))
+			# If one has an EmergencyAdvertisement name, investigate it
 			if "EmergencyAdvertisement" in name:
+				# Connect to the device
 				cli.prepare_device(address)
+				# Get characteristics of the device (its attributes)
 				chrcs = cli.device_characteristics()
 				for dev in chrcs:
 					# Go through the dict and inspect the UUIDs
-					if dev['uuid'].lower() == cli.RW_UUID.lower():
 						# If one of them is the same as the emergency UUID, allow it to talk to it
+					if dev['uuid'].lower() == cli.RW_UUID.lower():
+						# Get the handle of the service
 						handle = int(dev['handle'])
-						cli.write_value("Hello")
+						# Read a value from the server
 						data = cli.read_value()
 						print("Data from device: {}".format(bluezutils.from_byte_array(data)))
+						# Write the whole planned message to the server
+						cli.send_message()
 				# Otherwise, disconnect from device
 				cli.disconnect()
 	print("Done")
