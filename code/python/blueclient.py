a4f8c4c8124faf0915f635343200c963112574cc
Freddie Brown <fred@noser.net>
2020-02-07 15:39:21 +0000

    Testing writing

--- code/python/blueclient.py
+++ code/python/blueclient.py
@@ -2,7 +2,13 @@ import bluepy
 from bluepy.btle import Scanner, UUID, Peripheral, DefaultDelegate
 
 address = 'DC:A6:32:26:CE:70'
+SERVICE_UUID =  '0000FFF0-0000-1000-8000-00805f9b34fb'
+RW_UUID = '0000FFF1-0000-1000-8000-00805f9b34fb'
 scanner = Scanner()
 devices = scanner.scan(5.0)
 for dev in devices:
     print("Scan Data: {}".format(dev.getScanData()))
+peri = Peripheral(address)
+svc = peri.getServiceByUUID( SERVICE_UUID )
+ch = svc.getCharacteristics( char_uuid )[0]
+ch.write( "Hello" )
