38d5883e2ac0fa4c34ccc56c3c7d760a982159c3
Freddie Brown <fred@noser.net>
2020-02-11 16:41:09 +0000

    Added comments to advertising.py
    Commit will close issue #54

--- code/python/advertising.py
+++ code/python/advertising.py
@@ -47,6 +47,11 @@ class Advertisement(dbus.service.Object):
 		dbus.service.Object.__init__(self, bus, self.path)
 
 	def get_properties(self):
+        """
+        This method will collect the properties of the advert that are set, 
+        such as the type of advert, manufucturer data and local name, and will
+        return this as a dictionary of items which are set.
+        """
 		properties = {}
 		properties["Type"] = self.adtype
 		if self.service_uuids is not None:
@@ -73,41 +78,70 @@ class Advertisement(dbus.service.Object):
 		return {LE_ADVERTISEMENT_IFACE: properties}
 
 	def get_path(self):
+        """
+        This function will return the path to the advert object.
+        """
 		return dbus.ObjectPath(self.path)
 
 	def add_service_uuid(self, uuid):
+        """
+        Function to add a service UUID to the list of service UUIDs
+        """
 		if self.service_uuids is None:
 			self.service_uuids = []
 		self.service_uuids.append(uuid)
 
 	def add_solicit_uuid(self, uuid):
+        """
+        Function to add a solicit UUID to the list of solicit UUIDs
+        """
 		if self.solicit_uuids is None:
 			self.solicit_uuids = []
 		self.solicit_uuids.append(uuid)
 
 	def add_local_name(self, local_name):
+        """
+        Function to give a local name to the advert
+        """
 		self.local_name = dbus.String(local_name)
 
 	def tx_power(self, include):
+        """
+        Function to set whether the advertisement should include the 
+        transmission power of the device.
+        """
 		self.include_tx_power = dbus.Boolean(include)
 
 	def add_manufacturer_data(self, mancode, data):
+        """
+        Function to set the manufucturer data, part of the advertisement
+        """
 		if self.manufacturer_data is None:
 			self.manufacturer_data = dbus.Dictionary({}, signature='qv')
 		self.manufacturer_data[mancode] = dbus.Array(data, signature='y')
 
 	def add_service_data(self, uuid, data):
+        """
+        Function to set the service data, part of the advertisement
+        """
 		if self.service_data is None:
 			self.service_data = dbus.Dictionary({}, signature='sv')
 		self.service_data[uuid] = dbus.Array(data, signature='y')
 
 	def add_data(self, adtype, data):
+        """
+        Function to set other data, part of the advertisement
+        """
 		if self.data is None:
 			self.data = dbus.Dictionary({}, signature='yv')
 		self.data[adtype] = dbus.Array(data, signature='y')
 
 	@dbus.service.method(DBUS_PROP_IFACE, in_signature='s', out_signature='a{sv}')
 	def GetAll(self, interface):
+        """
+        Function to return the properties of the Advertisement. This is mainly 
+        used by DBUS so that it knows what the interface of the process is.
+        """
 		print('GetAll')
 		if interface != LE_ADVERTISEMENT_IFACE:
 			raise exceptions.InvalidArgsException()
@@ -116,12 +150,17 @@ class Advertisement(dbus.service.Object):
 
 	@dbus.service.method(LE_ADVERTISEMENT_IFACE, in_signature='', out_signature='')
 	def Release(self):
+        """
+        Function that is called when the advertisement is unregistered.
+        """
 		print("Advertisement Unregistered: %s" % (self.path))
 
 """
 This class takes the base advertising class and implements 
 more specific behaviours. These include specifying services 
 offered by the server, as well as service and manufacturer data. 
+This is an implementation of the Advertisement class that is defined 
+above.
 """
 class EmergencyAdvertisement(Advertisement):
 	def __init__(self, bus, index):
@@ -135,9 +174,16 @@ class EmergencyAdvertisement(Advertisement):
 		self.add_data(0x26, [0x01, 0x01, 0x00])
 
 def register_ad_cb():
+    """
+    Callback to be called whent the advertisement is registered.
+    """
 	print('Advertisement registered')
 
 def register_ad_error_cb(error):
+    """
+    Callback to be called when there is an error in registering 
+    advertisements.
+    """
 	print('Failed to register advertisement: ' + str(error))
 
 
