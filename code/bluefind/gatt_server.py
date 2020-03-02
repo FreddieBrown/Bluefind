efecd22384ab64840d47c1484823d960302eb84f
Freddie Brown <fred@noser.net>
2020-03-02 22:57:27 +0000

    working read issue out

--- code/bluefind/gatt_server.py
+++ code/bluefind/gatt_server.py
@@ -659,7 +659,10 @@ class SecureCharacteristic(Characteristic):
 			self.read_states[dev]['local'] += 1
 
 		print("Message being sent: {}".format(send_message))
-		return bluezutils.to_byte_array(send_message)
+		try:
+			return bluezutils.to_byte_array(send_message)
+		except Exception as e:
+			print("Error: {}".format(e))
 
 	"""
 This characteristic belongs to the EmergencyService. It has its own 
